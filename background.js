75cfe17eb918def02b698bc03493e60583479137
Henrik <henrinsk@student.matnat.uio.no>
2024-03-01 17:40:11 +0100

    Added comment - probably deprecated file

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-// background.js
+//leftover file, probably don't need this anymore
 browser.browserAction.onClicked.addListener((tab) => {
   browser.tabs.executeScript(tab.id, {
     file: "humblextractor.js"
